160f41d2c948f6dbfdf94854e2640effc69c99d6
ptavarez16 <ptavarez16@yahoo.com>
2018-04-12 19:31:10 -0400

    Add function in seeds.rb file to retrieve data - Function within seed.rb file retrieves data for week1 - New data folder to hold csv files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database
-# with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the
-# db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('data', 'week1.csv'))
+csv = CSV.parse(csv_text, :row_sep => :auto, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  row = row.to_hash
+  w = WeekOneRepo.new
+  w.week = row['Week']
+  w.day = row['Day']
+  w.repo_url = row['Repo Url']
+  w.name = row['Name']
+  w.category = row['Category']
+  w.save
+  puts "#{w.name} saved"
+end
+puts "There are now #{WeekOneRepo.count} rows in the Week One Repo table"
